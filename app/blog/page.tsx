8c81698fbd20a9f586b1cd3716f8148e2c6a4bc2
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-08-24 16:18:40 +0200

    added blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ export default function Blog(){
     return(
         <div className="min-vh-100 mt-3">
           <div className="text-center">
-          <h1 className="fw-bold display-1 text-black">Blog</h1>
+                <h1 className="fw-bold display-1 text-black">Blog</h1>
             <div className="container">
              
             <iframe src="https://mashkay.substack.com/embed"  frameBorder="0" scrolling="no"></iframe>
