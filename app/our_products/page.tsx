248685f6eec5f8d157e64e0b67ead7eb63af1096
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-08-27 19:38:43 +0200

    changes

--- app/our_products/page.tsx
+++ app/our_products/page.tsx
@@ -1,5 +1,40 @@
 "use client"
+import * as JsSearch from "js-search"
+import data from "../db/products.json"
+import { useState, useRef, useEffect } from "react";
+type Product = {
+    product_name: string;
+    product_img: string;
+    catalogue_link: string;
+    product_category: string;
+};
+
+type ProductList = Product[];
+
 export default function OurProducts(){
+    const [items, set_items] = useState<ProductList>([...data])
+    const inputRef:any= useRef(null)
+    const search = new JsSearch.Search("product_category")
+    search.indexStrategy = new JsSearch.AllSubstringsIndexStrategy()
+
+    search.addIndex("product_name")
+                search.addIndex("product_category")
+                search.addDocuments(data)
+    const handle_search=(search_value:string)=>{
+            if( search_value.length > 0 ){
+               
+                const search_results:any=search.search(search_value)
+                console.log(search_results)
+                if(search_results.length>0){
+                    set_items(search_results)
+                }
+            }else{
+                set_items(data)
+            }
+    }
+
+    
+   
     return(
         <div className="min-vh-100">
             <div className="text-center mt-5">
@@ -9,11 +44,32 @@ export default function OurProducts(){
 
             
             <div className="input-group">
-                <input className="form-control" type="text" placeholder="Find Part" />
-                <button className="btn secondary_background text-white">Search</button>
+                <input 
+                    className="form-control" 
+                    type="text" 
+                    ref={inputRef}
+                    placeholder="Find Part" 
+                    onChange={(e)=>handle_search(e.target.value)}
+                />
             </div>
-            <div className="results">
+            <div className="results d-flex flex-row flex-wrap gap-3 mt-4 justify-content-center">
+                {
+                    items.map((i,index)=>{
+                        return(
+                            <div className="border bg-light p-3  rounded" key={index} >
+                                <div className="text-center">
+                                    <img src="https://ngratesc.sirv.com/Mashkay/preview_item.png" width={200} loading="lazy" className="img-fluid img-thumbnail"/>
+                                </div>
+                                <div className="p-2 text-truncate">
+                                    <p className="fw-bold text-center">{i.product_category}</p>
+                                    <button className="btn secondary_button">View</button>
+                                </div>
+                                
 
+                            </div>
+                        )
+                    })
+                }
             </div>
 
             </div>
