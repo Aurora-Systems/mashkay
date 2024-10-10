9eb88e70007a4a1354e573ef2719dbe46257983f
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-10-10 15:19:05 +0200

    changes

--- app/our_products/yellow_machines/page.tsx
+++ app/our_products/yellow_machines/page.tsx
@@ -1,77 +1,104 @@
 import { bg_img } from "@/app/components/css_functions"
-export default function YellowMachines(){
-    return(
+export default function YellowMachines() {
+    return (
         <div className="mt-5">
-            
-        <div className="container">
 
-        <div className="row ">
-            <div className="col-sm text-center">
-                <h4>Yellow Machine Parts and Components
-                </h4>
-                <p>
-                At Mashkay Autoparts, we specialize in providing high-quality parts for heavy-duty yellow machines, including construction and earth-moving equipment. Whether you operate excavators, bulldozers, wheel loaders, or graders, our parts are built to endure the toughest environments, ensuring your machines are always ready for the job.
-                </p>
-                <p className="fw-bold">
-                Our range of yellow machine parts includes
+            <div className="container">
 
-
-
-                </p>
-                <div className="row gap-2"> 
-                    <div className="col-sm card mb-2">
-                        <div className="secondary_background w-100 rounded-bottom">
-                            <span className="text-white fw-bold">Engine Components</span>
-                        </div>
-                        <div style={{...bg_img("https://ngratesc.sirv.com/Mashkay/truck.png"), height:"45vh"}}>
-                        </div>
-                        <br/>
+                <div className="row ">
+                    <div className="col-sm text-center">
+                        <h4>Yellow Machine Parts and Components
+                        </h4>
                         <p>
-                        Keep your machines powered and efficient with our wide selection of engine parts, including pistons, gaskets, cylinder heads, and fuel injectors. We stock components for both diesel and gasoline engines, ensuring optimum performance for your heavy equipment.                        </p>
-                        <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on your yewllow machine engine parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
-                    </div>
-                    <div className="col-sm card mb-2">
-                    <div className="secondary_background w-100 rounded-bottom">
-                            <span className="text-white fw-bold">Hydraulic Systems</span>
-                        </div>
-                        <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/Custom-Aluminum-Dump-Truck-Lift-Hand-Car-Lift-Hydraulic-Cylinder-300-mm-Stroke-for-Farm-Equipment.webp"),height:"45vh"}}>
-                        </div>
-                        <br/>
-                        <p>                       
-                        nsure smooth and powerful operation with our extensive range of hydraulic parts. We supply pumps, cylinders, hoses, and fittings, allowing for reliable and precise movement of your machinery’s arms and attachments.
+                            At Mashkay Autoparts, we specialize in providing high-quality parts for heavy-duty yellow machines, including construction and earth-moving equipment. Whether you operate excavators, bulldozers, wheel loaders, or graders, our parts are built to endure the toughest environments, ensuring your machines are always ready for the job.
                         </p>
-                        <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on yellow machine hydraulic parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
+                        <p className="fw-bold">
+                            Our range of yellow machine parts includes
 
-                    </div>
-                    </div><div className="row gap-2">
-                    <div className="col-sm card mb-3">
-                    <div className="secondary_background w-100 rounded-bottom">
-                            <span className="text-white fw-bold">Transmission Parts</span>
-                        </div>
-                        <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/EV-Motor-360kw-550kw-Transmission-Amounted-Electric-Drive.webp"),height:"45vh"}}>
-                        </div>
-                        <br/>
-                        <p>For seamless power transfer and consistent operation, we provide high-quality transmission components. Our range includes gearboxes, torque converters, clutches, and drive shafts, all designed to keep your equipment running smoothly.                
+
+
                         </p>
-                        <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on yyellow machine transmission  parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
+                        <div className="row gap-2">
+                            <div className="col-sm card mb-2">
+                                <div className="secondary_background w-100 rounded-bottom">
+                                    <span className="text-white fw-bold">Engine Components</span>
+                                </div>
+                                <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/C829882.jpeg"), height: "45vh" }}>
+                                </div>
+                                <br />
+                                <p>
+                                    Keep your machines powered and efficient with our wide selection of engine parts, including pistons, gaskets, cylinder heads, and fuel injectors. We stock components for both diesel and gasoline engines, ensuring optimum performance for your heavy equipment.                        </p>
+                                <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on your yewllow machine engine parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
+                            </div>
+                            <div className="col-sm card mb-2">
+                                <div className="secondary_background w-100 rounded-bottom">
+                                    <span className="text-white fw-bold">Hydraulic Systems</span>
+                                </div>
+                                <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/Custom-Aluminum-Dump-Truck-Lift-Hand-Car-Lift-Hydraulic-Cylinder-300-mm-Stroke-for-Farm-Equipment.webp"), height: "45vh" }}>
+                                </div>
+                                <br />
+                                <p>
+                                    Ensure smooth and powerful operation with our extensive range of hydraulic parts. We supply pumps, cylinders, hoses, and fittings, allowing for reliable and precise movement of your machinery’s arms and attachments.
+                                </p>
+                                <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on yellow machine hydraulic parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
 
-                    </div> 
-                    <div className="col-sm card mb-3">
-                    <div className="secondary_background w-100 rounded-bottom">
-                            <span className="text-white fw-bold">Undercarriage Parts</span>
+                            </div>
+                        </div><div className="row gap-2">
+                            <div className="col-sm card mb-3">
+                                <div className="secondary_background w-100 rounded-bottom">
+                                    <span className="text-white fw-bold">Transmission Parts</span>
+                                </div>
+                                <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/EV-Motor-360kw-550kw-Transmission-Amounted-Electric-Drive.webp"), height: "45vh" }}>
+                                </div>
+                                <br />
+                                <p>For seamless power transfer and consistent operation, we provide high-quality transmission components. Our range includes gearboxes, torque converters, clutches, and drive shafts, all designed to keep your equipment running smoothly.
+                                </p>
+                                <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on yyellow machine transmission  parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
+
+                            </div>
+                            <div className="col-sm card mb-3">
+                                <div className="secondary_background w-100 rounded-bottom">
+                                    <span className="text-white fw-bold">Undercarriage Parts</span>
+                                </div>
+                                <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/Steel-Track-Chassis-Hydraulic-Undercarriage-System-0-5t-to-20t.webp"), height: "45vh" }}>
+                                </div>
+                                <br />
+                                <p>Keep your machines steady and durable on any terrain with our undercarriage solutions. We offer tracks, rollers, sprockets, idlers, and more, all engineered for maximum durability and wear resistance, even in the harshest conditions.</p>
+                                <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on your yellow machine undercarriage parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
+                            </div>
                         </div>
-                        <div style={{...bg_img("https://ngratesc.sirv.com/Mashkay/Steel-Track-Chassis-Hydraulic-Undercarriage-System-0-5t-to-20t.webp"),height:"45vh"}}>
+                    </div>
+
+
+                </div>
+                <div className="row gap-2 text-center">
+                            <div className="col-sm card mb-2">
+                                <div className="secondary_background w-100 rounded-bottom">
+                                    <span className="text-white fw-bold">Service Parts</span>
+                                </div>
+                                <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/128253.jpg"), height: "45vh" }}>
+                                </div>
+                                <br />
+                                <p>
+                                Ensure peak performance and longevity with our comprehensive range of service parts. From filters and seals to gaskets and hoses, our service parts are built to withstand the rigors of heavy-duty operations. Whether you&apos;re dealing with frequent maintenance or major overhauls, our components are designed to keep your yellow machines running smoothly and efficiently.                       </p>
+                                <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on your yewllow machine engine parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
+                            </div>
+                            <div className="col-sm card mb-2">
+                                <div className="secondary_background w-100 rounded-bottom">
+                                    <span className="text-white text-center fw-bold">Electrical Parts</span>
+                                </div>
+                                <div style={{ ...bg_img("https://ngratesc.sirv.com/Mashkay/cat_sensors.png"), height: "45vh" }}>
+                                </div>
+                                <br />
+                                <p>
+                                Power your yellow machines with reliable electrical components designed for optimal performance. We offer alternators, starters, sensors, wiring harnesses, and more, all engineered to handle the demanding electrical needs of heavy machinery. Keep your equipment operational in all conditions with our durable and high-performance electrical solutions.
+                                </p>
+                                <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on yellow machine hydraulic parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
+
+                            </div>
                         </div>
-                        <br/>
-                        <p>Keep your machines steady and durable on any terrain with our undercarriage solutions. We offer tracks, rollers, sprockets, idlers, and more, all engineered for maximum durability and wear resistance, even in the harshest conditions.</p>
-                        <a href="https://wa.me/263782255545?text=Good day, could i get more infomartion on your yellow machine undercarriage parts?" target="_blank"><button className="btn secondary_button text-white mb-2">Get Parts</button></a>
-                    </div>
-                    </div>
             </div>
-           
-        </div>
-        </div>
 
-    </div>
+        </div>
     )
 }
